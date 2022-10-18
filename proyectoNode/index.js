5d66cff6aa5a7e7fb45d005136aa271f81c508ca
AlejoPrzHub <josealeprz@gmail.com>
2022-10-18 18:03:21 +0200

    "retos"

---
+++ proyectoNode/index.js
@@ -0,0 +1,6 @@
+let suma = (op1,op2=0) => op1 + op2;
+let resta = (op1,op2=0) => op1 - op2;
+let division = (op1,op2=0) => op1/op2;
+let multi = (op1,op2) => op1*op2;
+let raizCua =(op1) => Math.sqrt(op1);
+module.exports = {suma,resta,division,multi,raizCua};
